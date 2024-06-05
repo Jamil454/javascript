4b090bc16a393a439d6b1ae5d283936b083a69d1
Hasanat1992 <151622839+Hasanat1992@users.noreply.github.com>
2024-06-05 13:09:04 +0600

    05June

---
+++ student.js
@@ -0,0 +1,9 @@
+const student_info = () => {
+    let name = "Rakib";
+    let address = "Mirpur";
+    return `Name: ${name} <br> Address: ${address}`;
+}
+ let country = "Bangladesh";
+let district = "Dhaka";
+export default student_info;
+export {district,country};

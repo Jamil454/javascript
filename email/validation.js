0624efcdcc29395377b5ff4cc196eca30187a223
Hasanat1992 <151622839+Hasanat1992@users.noreply.github.com>
2024-06-10 12:56:38 +0600

    10june

---
+++ email/validation.js
@@ -0,0 +1,15 @@
+class Validation {
+    constructor(email){
+        this.mail = email;
+    }
+
+    validate(){
+        let pattern = /[A-z0-9._]+@[A-z]+\.[A-z]/;
+        if (this.mail.search(pattern) == -1){
+            return " E-mail Address is wrong"
+        }
+        else {
+          return "E-mail Address is correct "
+        }
+    }
+}
